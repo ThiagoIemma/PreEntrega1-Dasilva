c95d8a814c12d56d83c647154851a4137c03f779
ThiagoIemma <tiemma@uade.edu.ar>
2024-10-07 18:49:12 -0300

    Archivo base

---
+++ index.js
@@ -0,0 +1,9 @@
+function mostrarProductos(){
+    let catalogo = "Catálogo de productos: \n  "
+    catalogo += "1. Iphone- $1200"
+    catalogo += "2. Ipad- $1500"
+    catalogo += "3. Macbook- $1900"
+    catalogo += "4. Airpods- $200"
+    catalogo += "5. Apllewatch- $500"
+    return catalogo
+}
