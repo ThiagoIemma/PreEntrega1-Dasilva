6b41704bbf571aeb2574f08bb56e4293092eb89e
ThiagoIemma <tiemma@uade.edu.ar>
2024-10-07 19:26:43 -0300

    se agrego html y funcion para mostar productos con IVA

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function mostrarProductos(){
-    let catalogo = "Catálogo de productos: \n  "
-    catalogo += "1. Iphone- $1200"
-    catalogo += "2. Ipad- $1500"
-    catalogo += "3. Macbook- $1900"
-    catalogo += "4. Airpods- $200"
-    catalogo += "5. Apllewatch- $500"
+    let catalogo = "Catálogo de productos: \n "
+    catalogo += "1. Iphone- $1200 \n ";
+    catalogo += "2. Ipad- $1500 \n ";
+    catalogo += "3. MacBook- $1900 \n ";
+    catalogo += "4. AirPods- $200 \n ";
+    catalogo += "5. AplleWatch- $500 \n ";
     return catalogo
 }
+
+function productosIva(){
+    let mirarProductos = true;
+    const precios = [1200, 1500, 1900, 200, 500];
+
+    while (mirarProductos === true){
+        console.log(mostrarProductos());
+        let producto = prompt(`Elige un producto(1-5) o escribe "Terminar" para finalizar:  `);
+        if (producto.toLowerCase() === "salir"){
+            mirarProductos = false;
+        } else {
+            const precio = precios[parseInt(producto)-1];
+            const precioConIva = precio * 1.21;
+            console.log(`El precio con IVA es: ${precioConIva}`);
+        }
+    }
+}
+
+productosIva();
